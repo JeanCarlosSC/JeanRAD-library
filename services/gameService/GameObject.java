c376da8711a076b907648849e25bed32809cfc75
Jean Carlos Santoya Cabrera <jeancarlosodi@gmail.com>
2020-08-29 17:07:30 -0500

    upgraded

--- services/gameService/GameObject.java
+++ services/gameService/GameObject.java
@@ -8,6 +8,12 @@ import javax.swing.JPanel;
  */
 public abstract class GameObject extends JPanel{
     
+    public GameObject(int x, int y){
+        setLocation(x, y);
+        setLayout(null);
+        setBackground(null);
+    }
+    
     public abstract void update();
     
 }
