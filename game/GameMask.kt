be9cadb252cc09f4cee0f445ad1d100d35484be3
JeanCarlos911 <jeancarlosodi@gmail.com>
2020-10-07 14:27:42 -0500

    new structure and focus

--- game/GameMask.kt
+++
@@ -1,7 +0,0 @@
-package services.gameService
-
-/**
- *
- * @author Jean Carlos Santoya Cabrera jeancarlosodi@gmail.com
- */
-class GameMask 
--- system/SystemMouse.java
+++
@@ -1,9 +0,0 @@
-package services.systemService;
-
-/**
- *
- * @author Jean Carlos Santoya Cabrera jeancarlosodi@gmail.com
- */
-public class SystemMouse {
-    
-}
