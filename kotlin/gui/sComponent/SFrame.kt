948baff3c064c34aa8349ba800b104343d9270ae
JeanCarlosSC <jeancarlosodi@gmail.com>
2021-08-25 11:28:36 -0500

    added kotlin version

---
+++ kotlin/gui/sComponent/SFrame.kt
@@ -0,0 +1,55 @@
+package lib.sRAD.kotlin.gui.sComponent
+
+import javax.swing.JFrame
+
+class SFrame : JFrame() {
+/*
+    public SFrame(int width, int height) {
+        setSize(width, height);
+    }
+*/
+    /*
+    public SFrame() {
+        super();
+    }*/
+    /*
+    public SFrame(int width, int height) {
+        setProperties(width, height, Theme.fBg, true, blackBorderTransparent, null, true, EXIT_ON_CLOSE, null);
+    }*/
+    /*
+    public void setProperties() {
+        setProperties(1280, 720, Theme.fBg, true, blackBorderTransparent, null, true, EXIT_ON_CLOSE, null);
+    }*/
+    /*
+    public void setProperties(int width, int height, Color background) {
+        setSize(width, height);
+        getContentPane().setBackground(background);
+    }*/
+    /*
+    public void setProperties(int width, int height, Color background, Boolean undecorated, Border border, Component relativeLocation) {
+        setProperties(width, height, background);
+        setUndecorated(undecorated);
+        rootPane.setBorder(border);
+        setLocationRelativeTo(relativeLocation);
+    }
+
+    public void setProperties(int width, int height, Color background, Boolean undecorated, Border border, Component relativeLocation,
+                              Boolean visible, int defaultCloseOperation, LayoutManager layout) {
+        setProperties(width, height, background, undecorated, border, relativeLocation);
+        setDefaultCloseOperation(defaultCloseOperation);
+        setLayout(layout);
+        setVisible(visible);
+    }
+
+    public void setMainBar(String title) {
+        setMainBar(title, "resources/sRAD/exampleLogo.png");
+    }
+
+    public void setMainBar(String title, String pathLogo) {
+        MainBar mainBar = new MainBar(this);
+        mainBar.setTitle(title);
+        mainBar.setLogo(new ImageIcon(pathLogo));
+        add(mainBar);
+        repaint();
+    }*/
+}
